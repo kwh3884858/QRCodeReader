f71d293f47c842d1117ffb6ec77f161097500ab1
Kong Wei Hang <kwh3884858@gmail.com>
2022-03-20 06:33:54 +0800

    Update resource files for vs2019.

--- Screenshot/resource.h
+++ Screenshot/resource.h
@@ -25,8 +25,8 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        146
-#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_RESOURCE_VALUE        148
+#define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
